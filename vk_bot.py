03dad37423bc6ae243f63c647091309e851e8931
Denis Mukhametov <denis.mukhametov@hmail.com>
2016-10-02 14:36:11 +0500

    add infinite loop

--- vk_bot.py
+++ vk_bot.py
@@ -2,14 +2,21 @@ import myqueue as queue_file
 import status_checker as status_checker_file
 import vk
 import os
+import time
 
-queue = queue_file.Queue()
-target = os.environ['target']
-listener = os.environ['listener']
-access_token = os.environ["access_token"]
 
-session = vk.Session(access_token=access_token)
-api = vk.API(session)
+def run():
+    queue = queue_file.Queue()
+    target = os.environ['target']
+    listener = os.environ['listener']
+    access_token = os.environ["access_token"]
 
-status_checker = status_checker_file.StatusChecker(queue, listener, target, api)
-status_checker.run()
+    session = vk.Session(access_token=access_token)
+    api = vk.API(session)
+
+    status_checker = status_checker_file.StatusChecker(queue, listener, target, api)
+    status_checker.run()
+
+run()
+while True:
+    time.sleep(10)
