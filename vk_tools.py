696818afc730c962d8b56216b52f50b1dc7da51b
Denis Mukhametov <denis.mukhametov@hmail.com>
2016-10-04 07:05:26 +0500

    fix get_last_messages

--- vk_tools.py
+++ vk_tools.py
@@ -62,7 +62,6 @@ async def send_message(target, message, queue, api):
 
 async def get_last_messages(user_id, queue, api):
     result = await queue.enqueue(api_get_last_messages, user_id, api)
-    print(result)
     messages = [(message["mid"], message["from_id"], message["body"]) for message in result if type(message) != int]
     messages.reverse()
     return messages
