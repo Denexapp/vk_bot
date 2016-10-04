b8662c2b62615cc712c93b89978bc41306f57fa3
Denis Mukhametov <denis.mukhametov@hmail.com>
2016-10-04 06:51:36 +0500

    keep playing with curio

--- vk_bot.py
+++ vk_bot.py
@@ -21,5 +21,8 @@ if __name__ == '__main__':
     status_checker = status_checker_file.StatusChecker(queue, listener, target, api)
     schedule = schedule_file.ScheduleBot(schedule_filename, schedule_dialogue, queue, api)
 
-    curio.spawn(status_checker.run())
-    curio.spawn(schedule.run())
+    async def tasks():
+        await curio.spawn(status_checker.run())
+        await curio.spawn(schedule.run())
+
+    curio.run(tasks())
