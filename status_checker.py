c386e18996b9c27b011063965c9ea64c29242edf
Denis Mukhametov <denis.mukhametov@hmail.com>
2016-10-04 05:28:17 +0500

    fix schedule

--- status_checker.py
+++ status_checker.py
@@ -17,7 +17,7 @@ class StatusChecker:
         print("StatusChecker: Received name {}".format(self.name))
         while True:
             status = await vk_tools.get_status(self.target, self.queue, self.api)
-            print("StatusChecker: {} still has status {}".format(status))
+            print("StatusChecker: {} still has status {}".format(self.name, status))
             if status is None:
                 self.status = status
             elif status != self.status:
